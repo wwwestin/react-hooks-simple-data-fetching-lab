bb9e8a92655f104e02e037a36f8f011748cbdd34
Westin Humble <westin.h@gmail.com>
2022-02-06 14:50:14 -0500

    Completed lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,20 @@
-// create your App component here
+import {useState, useEffect} from 'react';
+
+function App() {
+
+    const [dogPic, setDogPic] =useState(null);
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then((r) => r.json())
+        .then ((data) => {
+        setDogPic(data.message);
+        });
+    }, []);
+
+    if (!dogPic) return <p>Loading...</p>
+
+    return <img src= {dogPic} alt= "A Random Dog" />
+}
+
+export default App;
